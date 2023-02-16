36a834987faad5fbed50d1c6670ccd9fe1bc37a5
felipeaocampo <felipe.aocampo@gmail.com>
2023-02-16 10:37:05 -0500

    starting repo state

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require(`fs`);
+const url = require(`url`);
+const http = require(`http`);
+
+const server = http.createServer((req, res) => {
+  res.end(`Hello from server`);
+});
+
+server.listen(3000, () => {
+  console.log(`server is LISTENGING`);
+});
